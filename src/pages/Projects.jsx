25378b6ae597a118e6fc2df82584cb2244c41aee
Daniel Huynh <danielhuynh082@gmail.com>
2026-05-20 17:56:46 -0700

    import fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import ProjectCard from "./components/Projectcard";
+import ProjectCard from "../components/ProjectCard";
 import { desc, title } from "framer-motion/client";
 
 const Projects = () => {
