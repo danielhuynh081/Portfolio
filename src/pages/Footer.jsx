ed94954b9a3c8e49ea76b333caed19baa3df475a
Daniel Huynh <danielhuynh082@gmail.com>
2026-05-20 17:46:30 -0700

    Footer added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,10 +7,16 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center gap-6 text-[10px] font-bold uppercase tracking-[0.2em] text-slate-500">
           <span>© 2026 Daniel Huynh</span>
           <div className="flex space-x-8">
-            <a href="#" className="hover:text-accent transition-colors">
+            <a
+              href="https://github.com/danielhuynh081"
+              className="hover:text-accent transition-colors"
+            >
               GitHub
             </a>
-            <a href="#" className="hover:text-accent transition-colors">
+            <a
+              href="https://www.linkedin.com/in/danielhuynh6/"
+              className="hover:text-accent transition-colors"
+            >
               LinkedIn
             </a>
           </div>
